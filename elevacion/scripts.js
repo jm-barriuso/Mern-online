e710937abf64bc7ea4eb9392993b6ae33650bfd1
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-09-24 17:54:38 -0300

    elevacion

--- elevacion/scripts.js
+++ elevacion/scripts.js
@@ -89,5 +89,95 @@ console.log(food); */
 //console.log(food); //food no declare
 //console.log(food); //food no declare
 
+// 6
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+//AFTER HOISTING BY THE INTERPRETER
+// var gernre;
+// console.log(genre); undefine
+// genre = "disco";
+//function rewind() {
+//        var genre;
+//        genre = "rock";
+//        console.log(genre); log rock
+//        genre = "r&b";
+//       console.log(genre); log r&b
+//}
+// rewind();
+// console.log(genre); disco
 
+//7
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+//AFTER HOISTING BY THE INTERPRETER
+//        var dojo;
+//dojo = "san jose"; 
+//console.log(dojo); log San Jose
+//function learn() {
+//        var dojo;
+//        dojo = "seattle";
+//        console.log(dojo); log seattle
+//        dojo = "burbank";
+//        console.log(dojo); log burbank
+//    }
+//learn();
+//console.log(dojo); undefined
 
+// 8 
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+//AFTER HOISTING BY THE INTERPRETER
+//function makeDojo(name, students){
+//        const dojo = {};
+//        dojo.name = name;
+//        dojo.students = students;
+//        if(dojo.students > 50){
+//            dojo.hiring = true;
+//        }
+//        else if(dojo.students <= 0){
+//            dojo = "closed for now";
+//        }
+//        return dojo;
+//    }
+//console.log(makeDojo("Chicago", 65)); log {Chicago, 65, true}
+//function makeDojo(name, students){
+//        const dojo = {};
+//        dojo.name = name;
+//        dojo.students = students;
+//        if(dojo.students > 50){
+//            dojo.hiring = true;
+//        }
+//        else if(dojo.students <= 0){
+//            dojo = "closed for now";
+//        }
+//        return dojo;
+//    }
+// console.log(makeDojo("Berkeley", 0)); TypeError: Assignment to constant variable.

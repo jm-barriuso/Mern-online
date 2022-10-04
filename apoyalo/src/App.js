f2f50254bf5b172c52cd09d59b2c5c2b1de0168b
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-10-04 18:56:34 -0300

    apoyalo

---
+++ apoyalo/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  const personas =[
+    {firstName: 'Jane', lastName: 'Doe', age: 45 , hairColor: 'Black'},
+    {firstName: 'John', lastName: 'Smith', age: 88 , hairColor: 'Brown'},
+    {firstName: 'Millard', lastName: 'Fillmore', age: 50 , hairColor: 'Brown'},
+    {firstName: 'Maria', lastName: 'Smith', age: 62 , hairColor: 'Brown'},
+  ]
+  return (
+    <div className="App">
+      {personas.map((persona)=>(<PersonCard firstName={persona.firstName} lastName={persona.lastName} age={persona.age} hairColor={persona.hairColor}/> ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ apoyalo/src/components/MyNewComponents.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyNewComponents extends Component {
+    render() {
+        return (
+            <div>
+                hola
+            </div>
+        );
+    }
+}
+
+export default MyNewComponents;

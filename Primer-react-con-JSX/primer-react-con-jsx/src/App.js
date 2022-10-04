f6d9f7042f562471b819686908fb717906555001
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-10-04 14:29:32 -0300

    primer React con JSX

---
+++ Primer-react-con-JSX/primer-react-con-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I need to do:</h2>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Tun a marathont</li>
+      <li>Feed the dogst</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
